513defda93d2441098e4281095a085d471f14fe5
anandshivam44 <anand.shivam44@yahoo.com>
2019-12-10 21:46:40 +0530

    flow in android system

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("VIVZ","onCreate has started");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("VIVZ","onStart has started");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("VIVZ","onPause has started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("VIVZ","onResume has started");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("VIVZ","onStop has started");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("VIVZ","onDestroy has started");
     }
 }
